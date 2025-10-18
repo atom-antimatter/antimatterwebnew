f7607f1e59a3a6e6bb43e576e134c2d9a5207b67
Matthew Bravo <matthewbravo@Mac.lan>
2025-10-17 21:30:11 -0400

    Center services section vertically in viewport

--- src/components/ServiceSection.tsx
+++ src/components/ServiceSection.tsx
@@ -4,7 +4,7 @@ import BreakTitle from "./ui/BreakTitle";
 
 const ServiceSection = () => {
   return (
-    <div id="service-section">
+    <div id="service-section" className="min-h-screen flex items-center">
       <div className="flex w-full justify-end" id="services">
         <div className="w-full lg:w-3/5 flex flex-col gap-10 ">
           <div className="flex flex-col sm:flex-row gap-10 sm:gap-30 xl:gap-43 justify-between">
